63f540cbe2f4e03e2818b44ad4307a8237664ed0
siddhss5 <siddhss5@users.noreply.github.com>
2026-02-17 03:28:15 +0000

    deploy: bb800456811a6cd42fc14cb06c184b0a49a763a3

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,19 +1,19 @@
 var store = [{
         "title": "A Tangled Web, Slightly Untangled",
-        "excerpt":"I finally updated my website to the 21st century, and decided to go full Computer Scientist on it. I was looking for something that was full-featured (markdown, math), methodical to update (git), and customizable (layout, fonts). Having rewritten the RSS 2017 website from scratch (later enhanced by the incomparable Brian...","categories": ["Miscellaneous tricks"],
+        "excerpt":"I finally updated my website to the 21st century, and decided to go full Computer Scientist on it. I was looking for something that was full-featured (markdown, math), methodical to update (git), and customizable (layout, fonts). Having rewritten the RSS 2017 website from scratch (later enhanced by the incomparable Brian...","categories": ["miscellaneous-tricks"],
         "tags": ["web","jekyll","github"],
-        "url": "/miscellaneous%20tricks/an-untangled-web/",
+        "url": "/miscellaneous-tricks/an-untangled-web/",
         "teaser": null
       },{
         "title": "Writing Good Reviews",
-        "excerpt":"Reviewing papers is one of the most important roles of an academic. But yet, we provide surprisingly little guidance to our reviewers on the tone of their reviews. I tried to capture some of my thoughts in this email I sent to my reviewers when I ran RSS 2017. Dear...","categories": ["Academic advice"],
+        "excerpt":"Reviewing papers is one of the most important roles of an academic. But yet, we provide surprisingly little guidance to our reviewers on the tone of their reviews. I tried to capture some of my thoughts in this email I sent to my reviewers when I ran RSS 2017. Dear...","categories": ["academic-advice"],
         "tags": ["reviewing","writing","research"],
-        "url": "/academic%20advice/writing-good-reviews/",
+        "url": "/academic-advice/writing-good-reviews/",
         "teaser": null
       },{
         "title": "The Academic Research Flywheel",
-        "excerpt":"This post originally appeared on LinkedIn. Most of us suffer from a problem of infinite ambition and finite resources. A common mistake we make is to take the resource gunpowder we have, and make hundreds of small bullets and shoot. Some bullets miss, and some others hit. Instead, what if...","categories": ["Academic advice"],
+        "excerpt":"This post originally appeared on LinkedIn. Most of us suffer from a problem of infinite ambition and finite resources. A common mistake we make is to take the resource gunpowder we have, and make hundreds of small bullets and shoot. Some bullets miss, and some others hit. Instead, what if...","categories": ["academic-advice"],
         "tags": ["research","productivity"],
-        "url": "/academic%20advice/academic-research-flywheel/",
+        "url": "/academic-advice/academic-research-flywheel/",
         "teaser": null
       }]
